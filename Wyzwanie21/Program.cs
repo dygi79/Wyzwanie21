182c348038518f0e8f434d9587c11a45f0ab946f
dygi79 <bartekdygas@poczta.onet.pl>
2023-02-23 11:24:11 +0100

    zadanie z dnia 4

---
+++ Wyzwanie21/Program.cs
@@ -0,0 +1,29 @@
+﻿// zadaine dzien 4
+string name = "Ewa";
+int age = 33;
+bool famale = false; 
+
+if (famale == true && age < 30)
+{
+    Console.WriteLine("Kobieta poniezej 30 lat");
+}
+
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
+else if (famale == false && age < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
+
+else if (famale == false && age > 18)
+{
+    Console.WriteLine("Pelnoletni mezczyzna");
+}
+
+else
+{
+    Console.WriteLine("To nie wiem kim jest ten czlowiek");
+}
